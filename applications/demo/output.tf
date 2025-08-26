fa1452c81de4d9b54d0dbe849f7f830ffea9b540
l-chege <laurachege9@gmail.com>
2025-08-26 15:50:09 +0300

    remove old demo files

--- applications/demo/output.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "example-resources"
-    storage_account_name  = "examplestorage"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
